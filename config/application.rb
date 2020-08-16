6abd40b4c8dcf489b40f17f20ca0527f7f42d972
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-08-16 16:09:47 +0900

    初期設定終了

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FreeMarket
+module Fleamarket_sample_77a
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '54.95.229.43', user: 'ec2-user', roles: %w{app db web}
