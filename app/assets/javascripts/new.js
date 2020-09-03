1765781a7f520be59866941fa88b39254e2dae38
matsufuji06 <n.matsufuji06@gmail.com>
2020-09-03 23:52:10 +0900

    created "noteAlert" function at first

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,3 +1,16 @@
 $(function(){ 
-  
+  let note1 = $("input.content__description_name--input");
+
+  var noteBorder_css = {
+    border: "3px solid red",
+  }
+  var noteAlert_css = {
+    display: "block",
+  }
+  function noteAlert() {
+    $(this).css(noteBorder_css);
+    $("p.description_name_note").css(noteAlert_css);
+  }
+
+  note1.blur(noteAlert);
 });
