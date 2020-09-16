f438dbd5d04dab47b088018d855ef174cd99ee39
haruki913 <66477696+haruki913@users.noreply.github.com>
2020-09-16 12:45:53 +0900

    バリデーション について
    いろいろかけてみた

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  validates :user_id,:name,:price,:description,:condition,:shipment_fee,:shipment_region,:shipment_schedule,presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  # validates :price, {greater_than_or_equal_to: 9999999,less_than_or_equal_to: 300}
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
 end
