a259f172264c9ee10ce8c09f1797109724e03f56
matsufuji06 <n.matsufuji06@gmail.com>
2020-09-20 20:34:36 +0900

    items_controller.rbにdestroyアクションを定義（仮）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,12 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    end
+  end
 end
