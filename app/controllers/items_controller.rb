c506ce98f6d9a0a9ee38e57065a4f7f5ac1259d7
matsufuji06 <n.matsufuji06@gmail.com>
2020-09-21 23:20:34 +0900

    商品編集と削除リンクの設置＆showとdestroyアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,16 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path
     end
   end
