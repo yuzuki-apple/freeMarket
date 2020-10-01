be303b09899ccccecfdfe81ac2fc6fbc26ac34b7
haruki913 <66477696+haruki913@users.noreply.github.com>
2020-10-01 20:28:15 +0900

    エラーハンドリングについて
    いい感じにまとまってきた
    後少し

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,12 @@ class Item < ApplicationRecord
   validates :user_id,:name,:description,:category,:condition,:shipment_fee,:shipment_region,:shipment_schedule,presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category, presence: true
-  validates :condition, presence: true
-  validates :shipment_fee, presence: true
-  validates :shipment_region, presence: true
-  validates :shipment_schedule, presence: true
-  # validates_inclusion_of :price, in: 300..9_999_999, message: "300以上9999999以下で入力してください"
-  validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  # validates :images, length: { minimum: 1, maximum: 10, message: "の数が不正です" }
+  validates :category, numericality:{other_than: 0, message: "選択してください以外を選択してください"}
+  validates :condition, numericality:{other_than: 0, message: "選択してください以外を選択してください"}
+  validates :shipment_fee, numericality:{other_than: 0, message: "選択してください以外を選択してください"}
+  validates :shipment_region, numericality:{other_than: 0, message: "選択してください以外を選択してください"}
+  validates :shipment_schedule, numericality:{other_than: 0, message: "選択してください以外を選択してください"}
+  validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "300〜9,999,999円以内で入力してください"}
 
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
