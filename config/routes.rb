19f7a4072779360b52a9298b937c54686a9dee1b
yuzuki-apple <s1833307@nbu.ac.jp>
2020-10-06 20:49:16 +0900

    page-user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,18 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
+  # resource :users, only: [:show] do
+  # end
+  # resource :credits, only: [:index] do
+  # end
+  resources :users, only: [:show] do
+    member do
+      get :out
+    end
+  end
+  
+  resources :credits, only: [:index] 
+  
   resources :items, only: [:index, :show, :new, :edit, :create, :update, :destroy] do
     member do
       get :buy
