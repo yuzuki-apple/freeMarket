73920f434ded0c08f0c52405a33a44ce22756463
nagai0152 <nagai0152@gmail.com>
2020-10-13 21:44:23 +0900

    masterkey_change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_102308) do
+ActiveRecord::Schema.define(version: 2020_10_06_143941) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name_kanji", default: "", null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2020_08_28_102308) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
@@ -36,11 +45,39 @@ ActiveRecord::Schema.define(version: 2020_08_28_102308) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "category_id"
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "user_id", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "description", default: "", null: false
+    t.string "category", default: "", null: false
+    t.string "brand"
+    t.string "condition", default: "", null: false
+    t.integer "shipment_fee_id", null: false
+    t.string "shipment_region", default: "", null: false
+    t.string "shipment_schedule", default: "", null: false
+    t.integer "price", null: false
+    t.integer "stock", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "charge_id", null: false
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_payments_on_item_id"
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -61,5 +98,8 @@ ActiveRecord::Schema.define(version: 2020_08_28_102308) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "categories"
+  add_foreign_key "cards", "users"
+  add_foreign_key "images", "items"
+  add_foreign_key "payments", "items"
+  add_foreign_key "payments", "users"
 end
